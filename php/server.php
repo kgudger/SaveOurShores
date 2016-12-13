5a6d7bacafd8688fcd155f4f19a6345126d8243a
Keith Gudger <kgudger@gmail.com>
2016-12-13 08:46:43 -0800

    Fixed server side problem for iOS devices

--- php/server.php
+++ php/server.php
@@ -4,6 +4,19 @@
   header('Access-Control-Allow-Origin: *');
   $db = new DB();
 
+  /* Removes everything past a space, fixes problem with iOS requests */
+//  print_r($_REQUEST);
+  if ( is_array($_REQUEST) && count($_REQUEST) ) {
+    end($_REQUEST); 
+    $arend =  $_REQUEST[key($_REQUEST)];
+    $arend = explode(" ",$arend) ;
+    $arend = $arend[0] ;
+    $_REQUEST[key($_REQUEST)] = $arend ;
+    reset($_REQUEST) ; 
+//  echo "<br>Last element is " . $arend . "<br>" ;
+//  print_r($_REQUEST);
+  }
+  
   // get the command
   $command = $_REQUEST['command'];
 

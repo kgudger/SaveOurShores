6031b10004cf57034fb8d118ee86514442a7d149
Keith Gudger <kgudger@commumitytv.org>
2019-04-05 06:04:19 -0700

    iOS version 1.3.3 has all privacy requirements.

--- platforms/ios/www/js/app.js
+++ platforms/ios/www/js/app.js
@@ -11,7 +11,7 @@
  * @package    SaveOurShores
  *
  */
-	var Version = "1.3.0";
+	var Version = "1.3.3";
 	var currentLatitude = 0;
 	var currentLongitude = 0;
 	var options = {			// Intel GPS options

b5f8bf014ae1f8ccd4779d2ed1f5125d2c09c809
Keith Gudger <kgudger@gmail.com>
2021-05-03 12:57:04 -0700

    First pass at Spanish Language version

--- www/js/app.js
+++ www/js/app.js
@@ -11,7 +11,7 @@
  * @package    SaveOurShores
  *
  */
-	var Version = "1.3.10";
+	var Version = "1.4.0";
 	var currentLatitude = 0;
 	var currentLongitude = 0;
 	var options = {			// Intel GPS options
@@ -21,6 +21,16 @@
 	};
 	var Places = [];
 	var Stuff = {}; // Object / associative array to hold which item gets updated
+
+    var SummaryWords = {    // words for the summary page
+        uname: "User Name",
+        date:  "Date",
+        email: "Email",
+        hours: "Total Hours",
+        adults: "Adults",
+        youth:  "Youth",
+        area:   "Area Cleaned"
+    }; // object gets filled when language changes
 
 	var cats_done = false;
 	var event_done = false;
@@ -422,30 +432,30 @@ $(document).on("pagecontainerbeforeshow", function () {
 			for (var innerKey in obj[Key]) {
 				if ( obj[Key][innerKey] != 0 ) {
 					if ( innerKey == "name-in" ) {
-						myHTML+= "<tr><td>" + "User Name" +
+						myHTML+= "<tr><td>" + SummaryWords['uname'] +
 							"</td><td class='fright'>" + obj[Key][innerKey] + "</td></tr>";
 					} else if ( innerKey == "datein" ) {
 //						console.log( "In datein" );
-						myHTML+= "<tr><td>" + "Date" +
+						myHTML+= "<tr><td>" + SummaryWords['date'] +
 							"</td><td class='fright'>" + obj[Key][innerKey] + "</td></tr>";
 					} else if ( innerKey == "emailin" ) {
-						myHTML+= "<tr><td>" + "Email" +
+						myHTML+= "<tr><td>" + SummaryWords['email'] +
 							"</td><td class='fright'>" + obj[Key][innerKey] + "</td></tr>";
 					} else if ( innerKey == "hoursin" ) {
-						myHTML+= "<tr><td>" + "Total Hours" +
+						myHTML+= "<tr><td>" + SummaryWords['hours'] +
 							"</td><td class='fright'>" + obj[Key][innerKey] + "</td></tr>";
 					} else if ( innerKey == "adultsin" ) {
-						myHTML+= "<tr><td>" + "Adults" +
+						myHTML+= "<tr><td>" + SummaryWords['adults'] +
 							"</td><td class='fright'>" + obj[Key][innerKey] + "</td></tr>";
 					} else if ( innerKey == "youthin" ) {
-						myHTML+= "<tr><td>" + "Youth" +
+						myHTML+= "<tr><td>" + SummaryWords['youth'] +
 							"</td><td class='fright'>" + obj[Key][innerKey] + "</td></tr>";
 					} else if ( innerKey == "areain" ) {
-						myHTML+= "<tr><td>" + "Area Cleaned" +
+						myHTML+= "<tr><td>" + SummaryWords['area'] +
 							"</td><td class='fright'>" + obj[Key][innerKey] + "</td></tr>";
 					} else if ( !(innerKey.endsWith("in")) ) { // this is a problem waiting to happen
 						if (itemlist) {
-							myHTML+= "<tr><th>Item</th><th class='fright'>Amount</tr>";
+							myHTML+= "<tr><th>" + SummaryWords['item'] + "</th><th class='fright'>" + SummaryWords['amount'] + "</tr>";
 							itemlist = false;
 						}
 						console.log("i is " + i);
@@ -545,7 +555,7 @@ function sendfunc(params,test) {
                       window_alert.close();
                     }
                     let pval = document.getElementById("endMessage");
-                    pval.innerHTML = "<br>Thank you<br>for keeping<br>our beaches clean!";
+                    pval.innerHTML = "<br>Thank you<br>for keeping<br>our beaches clean!<br>Gracias por mantener<br>nuestras playas limpias";
                   }
                   else if (typeof (returnedList["place"]) !== 'undefined') {
 //                      alert("place is " + returnedList["place"] );
@@ -574,6 +584,13 @@ function sendfunc(params,test) {
                     val = document.getElementById("recycle")
                     val.value = returnedList["crecycle"];
 				  }
+                  else if (typeof (returnedList["language"]) !== 'undefined') {
+                          fillFormLang(returnedList);
+                  }  
+                  else if (typeof (returnedList["help"]) !== 'undefined') {
+                          fillText(returnedList);
+                          checkAndHide();
+                  }  
                   else {
 //                      alert(returnedList["Top Items"]);
 						if (Object.keys(returnedList).length === 0 && 
@@ -644,43 +661,19 @@ function fillForm(rList) {
     var myHTML = '<ul data-role="collapsible-set">';
     var option;
 //	var newHtml = "<div>" ;
+    let i = 0;
     for (var topKey in rList) {
-		myHTML+= '<li data-role="collapsible" data-inset="false" data-iconpos="right" class="setwidth"><h2 class="catheader">' + topKey + '</h2>';
-//		if ( topKey == "OTHER" ) {
-/*			myHTML += '<div class="item_field"> <label for "' + topKey + '"> <input data-role="none" type="number" class="right25" oninput = "other_change('+"'"+topKey+"'"+')" id="' + topKey + '" value="0" name="' + topKey + '" > <a href="#" class="blue_back ui-shadow ui-btn ui-corner-all ui-btn-inline ui-icon-minus ui-btn-icon-notext ui-btn-b ui-mini" onclick="other_minus_one(' + "'" + topKey + "'" + ')"></a> <a href="#" class="blue_back ui-shadow ui-btn ui-corner-all ui-btn-inline ui-icon-plus ui-btn-icon-notext ui-btn-b ui-mini" onclick="other_plus_one(' + "'" + topKey + "'" + ')"></a>';
-			myHTML += '<select name="'+topKey+'-field" id="'+topKey+'-field" data-inline="true" onChange="changeOther('+"'"+topKey+"'"+')"></select>';
-			Stuff[topKey] = "";
-//		}
-//        $('#formData').append(myHTML);
-        document.getElementById('formData').innerHTML+= myHTML;
-/*        if ( topKey != "OTHER" ) {*/
+		myHTML+= '<li data-role="collapsible" data-inset="false" data-iconpos="right" class="setwidth"><h2 class="catheader" id="olist_'+ i + '">' + topKey + '</h2>';
+		i+= 1;
+
 		for (var innerKey in rList[topKey]) {
-			var iVal = rList[topKey][innerKey] ;
-/*			myHTML+= '<li class="item_field"> <label for "' + iVal + '"> <input data-role="none" type="number" class="right25" id="' + iVal + '" value="0" name="' + iVal + '" >' + innerKey + '<a href="#" class="blue_back button_right ui-shadow ui-btn ui-corner-all ui-btn-inline ui-icon-minus ui-btn-icon-notext ui-btn-b ui-mini" onclick="minus_one(' + "'" + iVal + "'" + ')"></a> <a href="#" class="blue_back button_right ui-shadow ui-btn ui-corner-all ui-btn-inline ui-icon-plus ui-btn-icon-notext ui-btn-b ui-mini" onclick="plus_one(' + "'" + iVal + "'" + ')"></a></label></li>';*/
-//			myHTML+= '<div class="item_field"><div class="item_name">'+ innerKey + '</div><div class="fright"><div class="fleft"> <input class="left25" data-role="none" type="number" id="' + iVal + '" value="0" name="' + iVal + '" ></div><div class="fright item_right"><a href="#" class="blue_back button_right ui-shadow ui-btn ui-corner-all ui-btn-inline ui-icon-minus ui-btn-icon-notext ui-btn-b ui-mini" onclick="minus_one(' + "'" + iVal + "'" + ')"></a> <a href="#" class="blue_back button_right ui-shadow ui-btn ui-corner-all ui-btn-inline ui-icon-plus ui-btn-icon-notext ui-btn-b ui-mini" onclick="plus_one(' + "'" + iVal + "'" + ')"></a></div></div></div>';
+			var iVal = rList[topKey][innerKey] ; // db key value
+
 			iValNew = innerKey.replace(/ /g, "_") ;
 			inList.push(iValNew);
-			myHTML+= '<div class="item_field"><div class="item_name">'+ innerKey + '</div><div class="fright"><div class="fleft"> <input class="left25" data-role="none" type="number" onclick="this.select()" id="' + iValNew + '" value="0" name="' + iVal + '" ></div><div class="fright item_right"><a href="#" class="blue_back button_right ui-shadow ui-btn ui-corner-all ui-btn-inline ui-icon-minus ui-btn-icon-notext ui-btn-b ui-mini" onclick="minus_one(' + "'" + iValNew + "'" + ')"></a> <a href="#" class="blue_back button_right ui-shadow ui-btn ui-corner-all ui-btn-inline ui-icon-plus ui-btn-icon-notext ui-btn-b ui-mini" onclick="plus_one(' + "'" + iValNew + "'" + ')"></a></div></div></div>';
+			myHTML+= '<div class="item_field"><div class="item_name" id="' + iVal + '">'+ innerKey + '</div><div class="fright"><div class="fleft"> <input class="left25" data-role="none" type="number" onclick="this.select()" id="' + iValNew + '" value="0" name="' + iVal + '" ></div><div class="fright item_right"><a href="#" class="blue_back button_right ui-shadow ui-btn ui-corner-all ui-btn-inline ui-icon-minus ui-btn-icon-notext ui-btn-b ui-mini" onclick="minus_one(' + "'" + iValNew + "'" + ')"></a> <a href="#" class="blue_back button_right ui-shadow ui-btn ui-corner-all ui-btn-inline ui-icon-plus ui-btn-icon-notext ui-btn-b ui-mini" onclick="plus_one(' + "'" + iValNew + "'" + ')"></a></div></div></div>';
 		}
-//				document.getElementById('formData').innerHTML+= myHTML;
-    /*} else {
-			var fieldname = topKey+'-field';
-			var select = document.getElementById(fieldname);
-			option = document.createElement( 'option' );
-			option.value = 'empty';
-			option.text = 'Please Select';
-			select.add( option );
-			for (var innerKey in rList[topKey]) {
-				option = document.createElement( 'option' );
-				option.value = rList[topKey][innerKey];
-				option.text = innerKey;
-				select.add( option );
-				newHtml+= '<input id="' + rList[topKey][innerKey] + '" type="hidden" name="' + rList[topKey][innerKey] + '" value="0">';
-			}
-//		}
-    }*/
 		myHTML += "</li>";
-//		document.getElementById('formData').innerHTML+= newHtml;
 	}
 	myHTML += "</ul>";
 	var fdata = document.getElementById('formData') ;
@@ -702,13 +695,14 @@ function fillPlace(rList) {
     if ( place_no == 0 ) { // no place found 
 		option = document.createElement('option');
 		if ( ( currentLatitude == 0 ) || ( currentLongitude == 0 ) ) {
-			option.value = option.text = "Please Choose"
+			option.value = option.text = "Please Choose / Por favor seleccione"
 		} else {
-			option.value = option.text = "Automatic"
+			option.value = option.text = "OK"
 		}
 		select.add( option );
 		select.options[select.selectedIndex].value = option.value
 	}
+	Places = [];
     for (var topKey in rList['places']) {
 		option = document.createElement( 'option' );
 		option.value = option.text = topKey;
@@ -744,10 +738,12 @@ function fillPlace(rList) {
 function fillEvent(rList) {
     var myHTML = "" ;
     var select = document.getElementById('event-field');
+    removeOptions(select);
 	var option = document.createElement( 'option' );
 	option.value = 0;
-	option.text = "Please Select";
+	option.text = "Please Select/Por favor seleccione";
 	select.add( option );
+//	select.value = '0';
     for (var topKey in rList['results']) {
 //		console.log("Topkey in fillEvent is " + topKey);
 //		console.log("Innerkey in fillEvent is " + rList['results'][topKey]);
@@ -756,6 +752,7 @@ function fillEvent(rList) {
 		option.text = rList['results'][topKey];
 		select.add( option );
     }
+    select.selectedIndex = "0";
 }
 // fillEvent
 
@@ -857,6 +854,98 @@ function getCookie(cname) {
 	}
 	return name;
 }
+
+/** function to change the language of the app
+ *  param is the language value from the select
+ */
+function changeLang(chosen) {
+  console.log(chosen);
+  queryString = "command=getCatLang&lang=" + chosen;
+  sendfunc(queryString,true);
+  queryString = "command=getText&lang=" + chosen;
+  sendfunc(queryString,true);
+  queryString = "command=getEvent&lang=" + chosen;
+  sendfunc(queryString);
+  ready();
+}
+
+/**
+ *	Function to fill form with data from database
+ *  after changing the language
+ * @param rList is object returned from ajax
+ */
+function fillFormLang(rList) {
+    var myHTML = '';
+    var option;
+    let i = 0;
+    for (var topKey in rList) {
+        let category = document.getElementById("olist_" + i); // h2 element of category
+        if (category != null) {
+            let subcat = category.getElementsByClassName("ui-btn")[0];
+            if (subcat != null) {
+                let txtin = subcat.innerHTML ;
+                txtin = txtin.split('<span'); // grab up to <span and split
+                txtin[0]=topKey + "<span";     // add <span back in
+                subcat.innerHTML = txtin.join(''); // join together
+            }
+    		for (var innerKey in rList[topKey]) {
+    			let iVal    = rList[topKey][innerKey] ; // db key value
+			    let iValNew = innerKey.replace(/ /g, "_") ; // new inner id value
+    	        let val = document.getElementById(iVal); // div element w/ name
+    	        if (val != null) {
+    	            val.innerHTML = innerKey; // sets new name for item
+    	            // below changes input field name and plus and minus onclicks
+    	            // so that the summary page displays the new lang name
+    	            let valpar = val.parentNode; // val parent, no id
+    	            let valdiv = valpar.childNodes[1]; // next node, fright
+    	            let val2dv = valdiv.childNodes[0]; // first child, fleft
+       	            if (val2dv != null) { // should be input
+       	                let vall2i = val2dv.childNodes[0].nextSibling; // left25 input
+                        vall2i.id = iValNew; // give a new id
+                        let valr2a = valdiv.childNodes[1]; // 2nd child, fright
+                        let valr20 = valr2a.childNodes[0]; // should be minus
+                        valr20.setAttribute('onclick', 'minus_one(' + "'" + iValNew + "'" + ')');
+                        let valr21 = valr2a.childNodes[1]; 
+                        let valr21s = valr21.nextSibling;  // should be plus
+                        valr21s.setAttribute('onclick', 'plus_one(' + "'" + iValNew + "'" + ')');
+                    }
+                }
+            }
+        }
+        i+= 1;
+    }
+}
+
+/**
+ *	Function to change the app text with data from database
+ *  after changing the language
+ * @param rList is object returned from ajax
+ */
+function fillText(rList) {
+    for (var topKey in rList) {
+        if ( topKey == "SummaryWords" ) {
+            let sWords = rList['SummaryWords'];
+            SummaryWords = sWords;
+        } else {
+            let textf = document.getElementById(topKey); // div element of text
+            if (textf != null) {
+                textf.innerHTML = rList[topKey];
+            }
+        }
+    }
+}
+
+/**
+ *  Function to remove all elements from a select list
+ *  @param selectElement is the elements selected by id
+ */
+function removeOptions(selectElement) {
+   var i, L = selectElement.options.length - 1;
+   for(i = L; i >= 0; i--) {
+      selectElement.remove(i);
+   }
+}
+
 
 var Small = {
     'zero': 0,

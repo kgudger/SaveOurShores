c1c4630c08a5e1457cc8082050449877446a87b2
Keith Gudger <kgudger@gmail.com>
2024-01-23 00:11:07 -0800

    Release 1.5.2

--- www/js/app.js
+++ www/js/app.js
@@ -11,7 +11,7 @@
  * @package    SaveOurShores
  *
  */
-	var Version = "1.5.1";
+	var Version = "1.5.2";
 	var currentLatitude = 0;
 	var currentLongitude = 0;
 	var options = {			// Intel GPS options

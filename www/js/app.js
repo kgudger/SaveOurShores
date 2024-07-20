e9fe10452bcac9da637cb3b1cd9fb0fd519409dd
Keith Gudger <kgudger@gmail.com>
2024-07-20 15:22:50 -0700

    Upgraded to Android 35 and version 1.5.51

--- www/js/app.js
+++ www/js/app.js
@@ -7,11 +7,11 @@
  * @author Keith Gudger
  * @copyright  (c) 2015, Keith Gudger, all rights reserved
  * @license    http://opensource.org/licenses/BSD-2-Clause
- * @version    Release: 1.5.3
+ * @version    Release: 1.5.51
  * @package    SaveOurShores
  *
  */
-	var Version = "1.5.3";
+	var Version = "1.5.51";
 	var currentLatitude = 0;
 	var currentLongitude = 0;
 	var options = {			// Intel GPS options

77b548ecae7b8dbaa64573622f8e32cd01faf089
Keith Gudger <kgudger@gmail.com>
2025-05-22 12:39:23 -0700

    Updated Version for Play Store

--- www/js/app.js
+++ www/js/app.js
@@ -7,11 +7,11 @@
  * @author Keith Gudger
  * @copyright  (c) 2015, Keith Gudger, all rights reserved
  * @license    http://opensource.org/licenses/BSD-2-Clause
- * @version    Release: 1.5.53
+ * @version    Release: 1.5.61
  * @package    SaveOurShores
  *
  */
-	var Version = "1.5.6";
+	var Version = "1.5.61";
 	var currentLatitude = 0;
 	var currentLongitude = 0;
 	var options = {			// Intel GPS options
